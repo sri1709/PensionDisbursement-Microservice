59c22099c2cb065faa36d4277d417fa089f38fa6
sri1709 <58382463+sri1709@users.noreply.github.com>
2022-05-25 20:29:19 +0530

    Add files via upload

---
+++ PensionDisbursement-Microservice/src/main/java/com/cts/pension/disbursement/controller/PensionDisbursementController.java
@@ -0,0 +1,51 @@
+package com.cts.pension.disbursement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.pension.disbursement.exception.AadharNumberNotFound;
+import com.cts.pension.disbursement.exception.AuthorizationException;
+import com.cts.pension.disbursement.feignclient.AuthorisingClient;
+import com.cts.pension.disbursement.model.ProcessPensionInput;
+import com.cts.pension.disbursement.model.ProcessPensionResponse;
+import com.cts.pension.disbursement.service.PensionDisbursementServiceImpl;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@EnableEurekaClient
+@RequestMapping(value = "/api/v1")
+public class PensionDisbursementController {
+
+	@Autowired
+	private PensionDisbursementServiceImpl pensionDisbursementServiceImpl;
+	
+	@Autowired
+	private AuthorisingClient authorisingClient;
+	
+	@PostMapping("/disbursePension")
+	@ApiOperation(notes = "Returns the Process Pension Responce code", value = "Find Process Responce Code")
+	public ProcessPensionResponse getResponse(
+			@RequestHeader(value = "Authorization", required = true) String requestTokenHeader,
+			@RequestBody ProcessPensionInput processpensionInput) throws AuthorizationException, AadharNumberNotFound
+	{
+		System.out.println("In pension disbure=se controller");
+		if (authorisingClient.authorizeTheRequest(requestTokenHeader)) 
+		{
+			return pensionDisbursementServiceImpl.getResponce(requestTokenHeader,processpensionInput);
+		}
+		else
+		{
+			throw new AuthorizationException("Not allowed");
+		}
+		
+			
+	}
+	
+	
+}
