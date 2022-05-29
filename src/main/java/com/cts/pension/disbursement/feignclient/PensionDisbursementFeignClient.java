f8032d9cfdb7efc63c7e20e09c4d3c97bb57e5ac
sri1709 <58382463+sri1709@users.noreply.github.com>
2022-05-29 14:40:13 +0530

    Update PensionDisbursementFeignClient.java

--- src/main/java/com/cts/pension/disbursement/feignclient/PensionDisbursementFeignClient.java
+++ src/main/java/com/cts/pension/disbursement/feignclient/PensionDisbursementFeignClient.java
@@ -11,7 +11,7 @@ import com.cts.pension.disbursement.model.PensionerDetail;
 
 import io.swagger.annotations.ApiParam;
 
-@FeignClient(name ="PensionerDetail-Microservice",url = "http://localhost:8200/pensioner/api/v1")
+@FeignClient(name ="PensionerDetail-Microservice",url = "http://PMS-877629-Microservices-lb-1165068758.us-east-1.elb.amazonaws.com/pensioner/api/v1")
 public interface PensionDisbursementFeignClient {
 	
 	@GetMapping("/PensionerDetailByAadhaar/{aadharNumber}")
